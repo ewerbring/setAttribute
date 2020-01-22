cafb5ebef6a18500603c652ce147d36c8f7afe8f
Carl-Gustaf <ewerbring@gmail.com>
2020-01-22 15:50:27 -0500

    push

--- main.js
+++ main.js
@@ -5,6 +5,25 @@ function setSrc() {
   console.log(mySource);
 }
 
+function setSrc2() {
+  var iframe = document.querySelector("#iframe2");
+  var mySource = document.querySelector("#srcInput2").value;
+  iframe.setAttribute("src", mySource);
+  console.log(mySource);
+}
+function setSrc3() {
+  var iframe = document.querySelector("#iframe3");
+  var mySource = document.querySelector("#srcInput3").value;
+  iframe.setAttribute("src", mySource);
+  console.log(mySource);
+}
+function setSrc4() {
+  var iframe = document.querySelector("#iframe4");
+  var mySource = document.querySelector("#srcInput4").value;
+  iframe.setAttribute("src", mySource);
+  console.log(mySource);
+}
+
 ////find the input field withh its id
 ////store the content of the input field in a variable
 ////apple that variable to the source of the iframe
